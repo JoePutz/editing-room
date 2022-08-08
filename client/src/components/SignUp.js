1d3d37bfe90ed41fd733ef1cea2bd9743842ffc2
Joseph <josephputz89@gmail.com>
2022-08-08 16:42:57 -0500

    authentication

---
+++ client/src/components/SignUp.js
@@ -0,0 +1,85 @@
+function SignUp({ onLogin }) {
+    const [firstName, setFirstName] = useState("")
+    const [lastName, setLastName] = useState("")
+    const [email, setEmail] = useState("")
+    const [userImage, setUserImage] = useState("")
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [passwordConfirmation, setPasswordConfirmation] = useState("");
+  
+    function handleSubmit(e) {
+      e.preventDefault();
+      fetch("/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          first_name: firstName,
+          last_name: lastName,
+          username,
+          email,
+          image_url: userImage,
+          password,
+          password_confirmation: passwordConfirmation,
+        }),
+      })
+        .then((r) => r.json())
+        .then(onLogin);
+    }
+  
+    return (
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="first_name">First Name:</label>
+        <input
+          type="text"
+          id="first_name"
+          value={firstName}
+          onChange={(e) => setFirstName(e.target.value)}
+        />
+        <label htmlFor="last_name">Last Name:</label>
+        <input
+          type="text"
+          id="last_name"
+          value={lastName}
+          onChange={(e) => setLastName(e.target.value)}
+        />
+        <label htmlFor="username">Username:</label>
+        <input
+          type="text"
+          id="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <label htmlFor="email">Email:</label>
+        <input
+          type="text"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <label htmlFor="user_image">User Image:</label>
+        <input
+          type="text"
+          id="user_image"
+          value={userImage}
+          onChange={(e) => setUserImage(e.target.value)}
+        />
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <label htmlFor="password_confirmation">Confirm Password:</label>
+        <input
+          type="password"
+          id="password_confirmation"
+          value={passwordConfirmation}
+          onChange={(e) => setPasswordConfirmation(e.target.value)}
+        />
+        <button type="submit">Submit</button>
+      </form>
+    );
+  }
