f58ef58c48236a32a460bd2d410f60bae2c70b68
Joseph <josephputz89@gmail.com>
2022-08-11 18:37:28 -0500

    sign in out buttons

--- client/src/components/Genre.js
+++ client/src/components/Genre.js
@@ -1,22 +1,36 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import GenreCard from "./GenreCard";
 // import { Card } from 'semantic-ui-react'
 
 
 function Genre ()  {
+
+const crime = "Crime"
+const fantasy = "Fantasy"
+const romance = "Romance"
+const scifi = "Science Fiction"
+const horror = "Horror"
+const historical = "Historical"
+const literary = "Literary"
+const thriller = "Thriller"
+const western = "Western"
+const nonfi = "Non-Fiction"
+
     return (
         <>
         <div>Crime</div>
-        {/* <Link>Crime</Link>
-        <Link>Fantasy</Link>
-        <Link>Romance</Link>
-        <Link>Science Fiction</Link>
-        <Link>Horror</Link>
-        <Link>Historical</Link>
-        <Link>Literary</Link>
-        <Link>Thriller</Link>
-        <Link>Western</Link>
-        <Link>Non-Fiction</Link> */}
+        <Link to='/genres/:id'>Crime</Link>
+        <GenreCard genre={crime}/>
+        <GenreCard genre={fantasy}/>
+        <GenreCard genre={romance}/>
+        <GenreCard genre={scifi}/>
+        <GenreCard genre={horror}/>
+        <GenreCard genre={historical}/>
+        <GenreCard genre={literary}/>
+        <GenreCard genre={thriller}/>
+        <GenreCard genre={western}/>
+        <GenreCard genre={nonfi}/>
         </>
     )
 }
--- client/src/components/GenrePage.js
+++ client/src/components/GenrePage.js
@@ -1,6 +1,14 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-function GenrePage() {
+function GenrePage( {genre} ) {
+    const [stories, setStories] = useState([])
+
+    useEffect(() => {
+        fetch(`/stories?genre=${genre}`)
+          .then((response) => response.json())
+          .then((data) => setStories(data));
+    }, []);
+
     return (
         <h1>Should autofill the header with the genre selected then have an assortment of stories beneath in Story Cards that can be searched through</h1>
     )
